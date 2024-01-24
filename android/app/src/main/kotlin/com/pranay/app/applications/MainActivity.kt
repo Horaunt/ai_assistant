b273a8b002155ddf3c503e8668881f375b63d60e
Pranay Pandey <pranaypandey22254518@gmail.com>
2024-01-24 10:41:18 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/pranay/app/applications/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pranay.app.applications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:applications/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:applications/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'assistant',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        )
+      ),
+      home: const HomePage(),
+    );
+  }
+}
